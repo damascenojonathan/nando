ce7326bbaa2cff943e8743d896b6a5a5a2e7b006
damascenojonathan <damasceno.jonathan@escola.pr.gov.br>
2026-05-07 15:20:48 +0000

    aula 4

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ for (let i = 0; i < botoes.length; i++) {
     abas[i].classList.add("ativo");
   };
 }
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
